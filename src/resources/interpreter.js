cf537f64a6aa1f04f71fb801e2a3c5fdb8176a89
Bruce Li <isanoob1897@outlook.com>
2023-12-22 20:39:54 -0800

    add

--- src/resources/interpreter.js
+++ src/resources/interpreter.js
@@ -1,6 +1,21 @@
 function Compile(){
+    const htmlCode = document.getElementById('htmlCode').value;
+    const cssCode = document.getElementById('cssCode').value;
+    const jsCode = document.getElementById('jsCode').value;
 
-}
-function Display(){
-    
+    const resultFrame = document.createElement('iframe');
+    resultFrame.style.width = '100%';
+    resultFrame.style.height = '100%';
+    resultFrame.style.border = 'none';
+
+    document.body.appendChild(resultFrame);
+
+    const resultDocument = resultFrame.contentDocument;
+    const resultBody = resultDocument.body;
+
+    resultBody.innerHTML = `
+      <style>${cssCode}</style>
+      ${htmlCode}
+      <script>${jsCode}</script>
+    `;
 }
